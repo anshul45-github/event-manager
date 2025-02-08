0a45df38883a47089a22e59846b2ea9a1b88e9aa
Anshul <anshulmendiratta10d@gmail.com>
2025-02-08 22:32:58 +0530

    Event Edit Page UI - Implement event detail page and icon badge component - Add event validation, user authentication, and display completion status; create reusable icon badge component for UI consistency

--- app/(dashboard)/(routes)/organizer/events/[_id]/page.tsx
+++ app/(dashboard)/(routes)/organizer/events/[_id]/page.tsx
@@ -1,9 +1,56 @@
+import event from '@/lib/models/events';
+import user from '@/lib/models/userModel';
+
 import React from 'react';
 
-const EventIdPage = ({ params }: { params: { _id: string } }) => {
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { IconBadge } from '@/components/icon-badge';
+import { LayoutDashboard } from 'lucide-react';
+
+const EventIdPage = async ({ params }: { params: { _id: string } }) => {
+    const cookieStore = await cookies();
+    const token = cookieStore.get('token')?.value;
+    
+    const User = await user.findOne({ token: token });
+    
+    const Event = await event.findOne({ _id: params._id });
+
+    if(!Event)
+        return redirect("/");
+
+    if(Event.email !== User.email)
+        return redirect("/");
+
+    const requiredFields = [ Event.title, Event.description, Event.imageUrl, Event.time ];
+
+    const totalFields = requiredFields.length;
+    const completedFields = requiredFields.filter(Boolean).length;
+
+    const completionText = `(${completedFields}/${totalFields})`;
+
     return (
-        <div>
-            Course ID : {params._id}
+        <div className='p-6'>
+            <div className='flex items-center justify-between'>
+                <div className='flex flex-col gap-y-2'>
+                    <h1 className='text-2xl font-medium'>
+                        Event setup
+                    </h1>
+                    <span className='text-sm text-slate-700'>
+                        Complete all fields {completionText}
+                    </span>
+                </div>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
+                <div>
+                    <div className='flex items-center gap-x-2'>
+                        <IconBadge icon={LayoutDashboard} />
+                        <h2 className='text-xl'>
+                            Customize your event
+                        </h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ components/icon-badge.tsx
@@ -0,0 +1,58 @@
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import { LucideIcon } from "lucide-react";
+
+const BackgroundVariants = cva(
+    "rounded-full flex items-center justify-center",
+    {
+        variants: {
+            variant: {
+                default: "bg-sky-100",
+                success: "bg-emerald-100",
+            },
+            size: {
+                default: "p-2",
+                sm: "p-1",
+            }
+        },
+        defaultVariants: {
+            variant: "default",
+            size: "default",
+        }
+    }
+);
+
+const IconVariants = cva(
+    "",
+    {
+        variants: {
+            variant: {
+                default: "text-sky-700",
+                success: "text-emerald-700",
+            },
+            size: {
+                default: "h-8 w-8",
+                sm: "h-4 w-4",
+            },
+        },
+        defaultVariants: {
+            variant: "success",
+            size: "default",
+        }
+    }
+);
+
+type BackgroundVariantsProps = VariantProps<typeof BackgroundVariants>;
+type IconVariantProps = VariantProps<typeof IconVariants>;
+
+interface IconBadgeProps extends BackgroundVariantsProps, IconVariantProps {
+    icon: LucideIcon;
+};
+
+export const IconBadge = ({ icon: Icon, variant, size }: IconBadgeProps) => {
+    return (
+        <div className={cn((BackgroundVariants({ variant, size })))}>
+            <Icon className={cn(IconVariants({ variant, size }))} />
+        </div>
+    )
+}
